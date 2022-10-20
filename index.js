1ed0a7143660de2adac5ddb6d14e9691832265b7
Nana Adu Pokuaah <nana.pokuaah@bison.howard.edu>
2022-10-19 22:31:14 -0400

    commit tree 656d2964afbe27e83e0e9a245e821bbfb09112f7

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(activity="roller-skate"){// Had return as This saturday I want to roller-skate before this
+   return( `This Saturday, I want to ${activity}!`)
+}
+
+function mondayWork(plan="go to the office"){
+    return `This Monday, I will ${plan}.`
+    //return ("This Monday, I will go to the office."")
+}
+
+function wrapAdjective(outerValue ="*"){
+    return function (innerValue = "special"){
+        return `You are ${outerValue}${innerValue}${outerValue}!`
+    }
+
+}
